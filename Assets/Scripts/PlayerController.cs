06c50c494816436d25de05bebf21adbbc8ffee3a
ICETIANE\Tiane <tiane.erwee@gmail.com>
2018-08-11 16:00:48 +0200

    UI input panel
    Made changes in nonARscene and created assets for input panel. Tested it on phone.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,84 +6,88 @@ using Vuforia;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-	private Rigidbody rigidbody;
+    private Rigidbody rigidbody;
+    [SerializeField]
+    private float movespeed = 5;
+    [SerializeField]
+    private float jumpHeight = 10;
+    [SerializeField]
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField]
+    private LayerMask jumpableMask;
+    [SerializeField]
+    private CapsuleCollider collider;
+    [SerializeField]
+    private Transform gravityWell;
+
+    private bool canJump = false;
+    private Vector3 fakeGravity;
+
+    [SerializeField]
+    private Camera mainCamera;
+
 	[SerializeField]
-	private float movespeed = 5;
-	[SerializeField]
-	private float jumpHeight = 10;
-	[SerializeField]
-	private Vector3 velocity = Vector3.zero;
-	[SerializeField]
-	private LayerMask jumpableMask;
-	[SerializeField]
-	private CapsuleCollider collider;
-	[SerializeField]
-	private Transform gravityWell;
+	private ToggleStickBehaviour toggleStick;
 
-	private bool canJump = false;
-	private Vector3 fakeGravity;
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
+    }
 
-	[SerializeField]
-	private Camera mainCamera;
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.gravity = gravityWell.up * -9.8f;
 
-	// Use this for initialization
-	void Start()
-	{
-		rigidbody = GetComponent<Rigidbody>();
-		collider = GetComponent<CapsuleCollider>();
-	}
+        velocity = (toggleStick.Horizontal() * gravityWell.right) + (toggleStick.Vertical() * gravityWell.forward);
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            Jump();
+            canJump = false;
+        }
+        RotateGravityWell();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		Physics.gravity = gravityWell.up * -9.8f;
-#if UNITY_EDITOR
-		velocity = (Input.GetAxisRaw("Horizontal") * gravityWell.right) + (Input.GetAxisRaw("Vertical") * gravityWell.forward);
-#else
-		velocity = (Input.GetAxis("Horizontal") * gravityWell.right) + (Input.GetAxis("Vertical") *  gravityWell.forward);
-#endif
-		if (Input.GetButtonDown("Jump") && canJump)
-		{
-			Jump();
-			canJump = false;
-		}
-		RotateGravityWell();
-	}
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + (velocity.normalized * movespeed * Time.deltaTime));
+    }
 
-	private void FixedUpdate()
-	{
-		rigidbody.MovePosition(transform.position + (velocity.normalized * movespeed * Time.deltaTime));
-	}
+    public void Jump()
+    {
+        if (canJump)
+        {
+            rigidbody.AddForce(gravityWell.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            canJump = false;
+        }
+    }
 
-	private void Jump()
-	{
-		rigidbody.AddForce(gravityWell.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
-	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform.tag == "Ground")
+        {
+            canJump = true;
+            Ground gc = collider.gameObject.GetComponent<Ground>();
+            if (gc != null)
+            {
+                gravityWell = gc.GetGravityWell();
+            }
+        }
+    }
 
-	private void OnCollisionEnter(Collision collision)
-	{
-		if (collision.gameObject.transform.tag == "Ground")
-		{
-			canJump = true;
-			Ground gc = collider.gameObject.GetComponent<Ground>();
-			if (gc != null)
-			{
-				gravityWell = gc.GetGravityWell();
-			}
-		}
-	}
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.transform.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
 
-	private void OnCollisionExit(Collision collision)
-	{
-		if (collision.gameObject.transform.tag == "Ground")
-		{
-			canJump = false;
-		}
-	}
-
-	private void RotateGravityWell()
-	{
-		//gravityWell.transform.LookAt(transform.position + transform.TransformDirection(localPos));
-		gravityWell.right = mainCamera.transform.right;
-	}
+    private void RotateGravityWell()
+    {
+        //gravityWell.transform.LookAt(transform.position + transform.TransformDirection(localPos));
+        gravityWell.right = mainCamera.transform.right;
+    }
 }
---
+++ Assets/Scripts/ToggleStickBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ToggleStickBehaviour : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+
+    private Vector2 target;
+    private Vector2 home;
+    [SerializeField]
+    private Image backgroundImage;
+    [SerializeField]
+    private Image joystickImage;
+    private Vector3 input;
+
+    void Start()
+    {
+        home = this.gameObject.transform.position;
+        backgroundImage = this.GetComponent<Image>();
+        joystickImage = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 position;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(backgroundImage.rectTransform
+                                                                    , eventData.position
+                                                                    , eventData.pressEventCamera
+                                                                    , out position))
+        {
+            position.x = (position.x / backgroundImage.rectTransform.sizeDelta.x);
+            position.y = (position.y / backgroundImage.rectTransform.sizeDelta.y);
+
+            input = new Vector3(position.x * 2 - 1, 0, position.y * 2 - 1);
+            input = input.magnitude > 1.0f ? input.normalized : input;
+
+            joystickImage.rectTransform.anchoredPosition =
+                new Vector3(input.x * (backgroundImage.rectTransform.sizeDelta.x / 3)
+                            , input.z * (backgroundImage.rectTransform.sizeDelta.y / 3));
+
+            Debug.Log(input);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        input = Vector3.zero;
+        joystickImage.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public float Horizontal()
+    {
+        if (input.x != 0)
+        {
+            return input.x;
+        }
+        else
+        {
+#if UNITY_EDITOR
+            return Input.GetAxisRaw("Horizontal");
+#else
+			return Input.GetAxis("Horizontal");
+#endif
+        }
+    }
+
+    public float Vertical()
+    {
+		if (input.z != 0)
+        {
+            return input.z;
+        }
+        else
+        {
+#if UNITY_EDITOR
+            return Input.GetAxisRaw("Vertical");
+#else
+			return Input.GetAxis("Vertical");
+#endif
+        }
+    }
+}
